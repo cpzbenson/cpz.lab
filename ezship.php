59b0f1cfd697f520891c211975bcc620985b9a8b
Benson <cpz,benson@gmail.com>
2013-07-26 12:31:22 +0800

    0726

---
+++ ezship.php
@@ -0,0 +1,31 @@
+﻿<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<?
+	//echo $_GET['st_name'];
+	
+	echo mb_detect_encoding("陳品任");
+	//echo mb_convert_encoding("陳品任",'UTF-8', 'big5')
+?>
+陳品任<br/>
+
+
+	<form id="storeForm" class="form-inline" method="post" action ="http://www.ezship.com.tw/emap/rv_request_web.jsp">
+		<input type="hidden" name="su_id" value="lingabee@talkin.cc" />
+		<input type="hidden" name="order_id" value="13566" />
+		<input type="hidden" name="rturl" value="http://stage.lingabee.com/ezship.php" />
+		<input type="hidden" name="rv_name" value="<?echo mb_convert_encoding("陳品任",'UTF-8', 'big5')?>" />
+		<input type="hidden" name="rv_email" value="cpz.benson@gmail.com" />
+		<input type="hidden" name="rv_mobil" value="0966389889" />
+		<input type="hidden" name="rv_amount" value="200" />
+		<input type="submit" value="123"/>
+	</form>
+</body>
+
+
+<iframe width="100%" height="300" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=23.697810,120.960515&amp;ie=UTF8&amp;t=m&amp;z=14&amp;&amp;output=embed"></iframe>
+
+</html>
--- kkk.php
+++ kkk.php
@@ -6,8 +6,26 @@
 		$("#test").click(function(){
 			window.history.pushState({}, document.title, "ppp.php?ket=123");
 		})
+		map.enableScrollWheelZoom();
+		google.maps.event.addListener(map, 'click', function(event){
+			var mapOptions = {
+				draggable: true,
+				scrollwheel:true
+			};
+		});
 	})	
 </script>
 
 
-<div id="test">點我</div>
+<div id="test">點我</div>...
+<div width="100%">
+	<div style="display: inline-block; width: 30%">
+	qweqwe
+	</div>
+
+	<div style="display: inline-block; width: 68%">
+		<iframe width="100%" height="300" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=24.9889,121.311&amp;ie=UTF&amp;output=embed"></iframe>
+		
+		<iframe width="425" height="350" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=24.9889,121.311&amp;ie=UTF8&amp;ll=24.9889,121.311&amp;spn=0.011961,0.013797&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+	</div>
+</div>
