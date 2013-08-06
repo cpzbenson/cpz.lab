30941ca48080590df6152a9a1abd8147ba4603f7
Benson <cpz.benson@gmail.com>
2013-08-06 14:33:59 +0800

    0806

---
+++ 0731.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>登入</title>
+		<!--<link rel="stylesheet" type="text/css" href="styles/login_regi.css" />-->
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script>
+		</script>
+		
+		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
+        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="http://jquery-watermark.googlecode.com/svn-history/r53/trunk/jquery-watermark/jquery.watermark/jquery.watermark.min.js" type="text/javascript"></script>
+		
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=9"></meta>
+		<script>
+			$(document).ready(function(){
+				$('body').css('color', 'red');
+            });
+		</script>
+		<style type="text/css" >
+		</style>
+	</head>
+			
+	<body>
+		<div id="myCarousel" class="carousel slide" style="width: 1005px;">
+			<div class="carousel-inner">
+				<div class="active item">
+					<img src="banner.png" alt="" />
+				</div>
+			</div>
+			<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+			<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</div>
+	</body>
+</html>
