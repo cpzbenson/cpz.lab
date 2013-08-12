308ba3f35de2d144c9e1ac091a803de89eeb7e2b
Benson <cpz,benson@gmail.com>
2013-08-12 23:51:44 +0800

    080223

---
+++ 0812.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="keywords" content="拎咖啡, Lingabee, 耳掛式咖啡, 濾泡式咖啡" />
+		<meta name="description" content="拎咖啡 Lingabee，30 秒讓您品嘗現泡" />
+		<meta http-equiv="Content-Language" content="zh-tw" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>登入</title>
+		<link rel="stylesheet" type="text/css" href="styles/login_regi.css" />
+		<meta http-equiv="X-UA-Compatible" content="IE=9"></meta>
+		
+		
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
+        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js" type="text/javascript"></script>    
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+		
+		
+		<!--
+		<script src="http://jquery-watermark.googlecode.com/svn-history/r53/trunk/jquery-watermark/jquery.watermark/jquery.watermark.min.js"></script>
+		-->
+		<script>
+			$(document).ready(function(){
+				//$('body').css('color', 'red');
+            });
+		</script>
+		<style type="text/css" >
+		.casd{
+			
+		}
+		</style>
+	</head>
+			
+	<body  style="width:800px; margin: 0 auto">
+			
+			<style type="text/css">
+				
+			</style>
+
+			<div class="zoom">
+			   <img src="http://goo.gl/DSKDpJ" width="750px"/>
+			   <p>
+			   <h4>【賣多少送多少，一起來拿購物金】</h4>
+				
+				2013/9/18 日前，於 Lingabee 網站訂購前 200 盒者<br>
+				每筆訂單皆享有 8/15 至 9/18 拎咖啡銷售總盒數相同數目之購物金！<br><br>
+
+				狀況1：若 8/15 至 9/18 拎咖啡賣出 100 盒，活動期間每筆訂單，皆享有 100 元購物金<br>
+				狀況2：若 8/15 至 9/01 拎咖啡賣出 200 盒，活動期間每筆訂單，皆享有 200 元購物金，活動提早結束<br><br>
+
+				還在等什麼！一起相揪來拿購物金！
+				</p>
+			</div>
+
+
+
+	</body>
+
+
+
+
+	</body>
+</html>
---
+++ html_example.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="keywords" content="拎咖啡, Lingabee, 耳掛式咖啡, 濾泡式咖啡" />
+		<meta name="description" content="拎咖啡 Lingabee，30 秒讓您品嘗現泡" />
+		<meta http-equiv="Content-Language" content="zh-tw" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>登入</title>
+		<link rel="stylesheet" type="text/css" href="styles/login_regi.css" />
+		<meta http-equiv="X-UA-Compatible" content="IE=9"></meta>
+		
+		
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
+        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js" type="text/javascript"></script>    
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+		
+		
+		<!--
+		<script src="http://jquery-watermark.googlecode.com/svn-history/r53/trunk/jquery-watermark/jquery.watermark/jquery.watermark.min.js"></script>
+		-->
+		<script>
+			$(document).ready(function(){
+				//$('body').css('color', 'red');
+            });
+		</script>
+		<style type="text/css" >
+		.casd{
+			
+		}
+		</style>
+	</head>
+			
+	<body>
+		<html>    
+			
+			<style type="text/css">
+				.zoom { 
+				   width: 200px; padding: 5px; border: 1px solid black; 
+				   -webkit-transition: all .3s ease-out; 
+				   -moz-transition: all .3s ease-out; 
+				   -o-transition: all .3s ease-out; 
+				   transition: all .3s ease-out;
+				}
+				.zoom:hover { 
+				   -moz-transform: scale(2);
+				   -webkit-transform: scale(2);
+				   -o-transform: scale(2);
+				   transform: scale(2);
+				   -ms-transform: scale(2);
+					filter: progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=2, M12=-0, M21=0, M22=2);
+			   }
+			</style>
+
+			<div class="zoom">
+			   <img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-frc1/249140_10152905850200092_1927305133_n.jpg"/>
+			</div>
+
+
+
+		</html>
+	</body>
+
+
+
+
+	</body>
+</html>
